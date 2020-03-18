668e9115226d71e22423b38028a2bd3798c85de3
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-03-17 22:08:14 -0400

    form completed, submit handler working

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
+
 import logo from './logo.svg';
+import Form from './Form';
+
 import './App.css';
 
 function App() {
   
   const [teamMembers, setTeamMembers] = useState([
     {
-    name: "Johnny Appleseed",
+    id: "1",
+    memberName: "Johnny Appleseed",
     email: "seedyJ@icloud.com",
     role: "Back-End"
     },
     {
-    name: "Joe Camel",
+    id: "2",
+    memberName: "Joe Camel",
     email: "smokyhumps@camel.com",
     role: "Front-End"
     },
     {
-    name: "Willy Wonka",
+    id: "3",
+    memberName: "Willy Wonka",
     email: "candymancandymancandyman@wonka.com", 
     role: "React Developer"
     }
@@ -24,30 +30,35 @@ function App() {
   
   ]);
   
+  const addTeamMemberHandler = newMember => {
+    setTeamMembers([...teamMembers, newMember])
+
+  } 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
         {
-        teamMembers.map((item) => {
-          return(
-          <div className="member_container">
-            <p>
-             Name: {item.name}
-            </p>
-            <p>
-             Email: {item.email}
-           </p>
-            <p>
-             Role: {item.role}
-            </p>
-          </div>
-          )        
-        })
-        
+          teamMembers.map((item) => {
+            return(
+            <div className="member_container" >
+              <p>
+              Name: {item.memberName}
+              </p>
+              <p>
+              Email: {item.email}
+              </p>
+              <p>
+              Role: {item.role}
+              </p>
+            </div>
+            )        
+          })
         }
       </header>
+      <Form addMember={addTeamMemberHandler}/>
     </div>
   );
 }
