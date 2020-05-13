8e8029e17338f15dfce489adc695d3b0d9c64c2e
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-05-12 20:56:15 -0400

    added edit buttons not yet functional

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,64 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import logo from './logo.svg';
-import Form from './Form';
+import logo from "./logo.svg";
+import Form from "./Form";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  
+  /* const [memberToEdit, setMemberToEdit] = useState({
+    id: "",
+    memberName: "",
+   /email: "",
+    role: "",
+  });
+  */
   const [teamMembers, setTeamMembers] = useState([
     {
-    id: "1",
-    memberName: "Johnny Appleseed",
-    email: "seedyJ@icloud.com",
-    role: "Back-End"
+      id: "1",
+      memberName: "Johnny Appleseed",
+      email: "seedyJ@icloud.com",
+      role: "Back-End",
     },
     {
-    id: "2",
-    memberName: "Joe Camel",
-    email: "smokyhumps@camel.com",
-    role: "Front-End"
+      id: "2",
+      memberName: "Joe Camel",
+      email: "smokingjoe@camel.com",
+      role: "Front-End",
     },
     {
-    id: "3",
-    memberName: "Willy Wonka",
-    email: "candymancandymancandyman@wonka.com", 
-    role: "React Developer"
-    }
-
-  
+      id: "3",
+      memberName: "Willy Wonka",
+      email: "candyman@wonka.com",
+      role: "React Developer",
+    },
   ]);
-  
-  const addTeamMemberHandler = newMember => {
-    setTeamMembers([...teamMembers, newMember])
 
-  } 
+  const addTeamMemberHandler = (newMember) => {
+    setTeamMembers([...teamMembers, newMember]);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        {
-          teamMembers.map((item) => {
-            return(
-            <div className="member_container" >
-              <p>
-              Name: {item.memberName}
-              </p>
-              <p>
-              Email: {item.email}
-              </p>
-              <p>
-              Role: {item.role}
-              </p>
+
+        {teamMembers.map((item) => {
+          //const editTeamMemberHandler = (item) => {
+           //setMemberToEdit({ [item.keys]: [item.values] });
+            //console.log(memberToEdit);
+          
+          return (
+            <div className="member_container">
+              <p>Name: {item.memberName}</p>
+              <p>Email: {item.email}</p>
+              <p>Role: {item.role}</p>
+              <button
+                className="edit_btn"
+                //onClick={editTeamMemberHandler(item)}
+              >
+                Edit
+              </button>
             </div>
-            )        
-          })
-        }
+          );
+        })}
       </header>
-      <Form addMember={addTeamMemberHandler}/>
+      <Form addMember={addTeamMemberHandler} />
     </div>
   );
 }
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,68 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function Form (props) {
-    
-    const [formState, setFormState] = useState({});
+function Form(props) {
+  const [formState, setFormState] = useState({});
 
-    const formChangeHandler = event => {
-        setFormState( {...formState, [event.target.name]: event.target.value })
-    }
-    const formSubmitHandler =  event => {
-        
-        event.preventDefault();
+  const formChangeHandler = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  };
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
 
-        props.addMember({
-            ...formState, 
-            id: Date.now(),
-            memberName: formState.memberName,
-            email: formState.email,
-            role: formState.role
-        })
+    props.addMember({
+      ...formState,
+      id: Date.now(),
+      memberName: formState.memberName,
+      email: formState.email,
+      role: formState.role,
+    });
+  };
 
+  return (
+    <div className="Form">
+      <form onSubmit={formSubmitHandler}>
+        <label htmlFor="memberName">Name: </label>
+        <input
+          type="text"
+          name="memberName"
+          value={formState.memberName}
+          placeholder="Enter a name"
+          onChange={formChangeHandler}
+        />
 
-    }
+        <br></br>
 
+        <label htmlFor="email">Email: </label>
+        <input
+          type="text"
+          value={formState.email}
+          name="email"
+          placeholder="Enter an Email Address"
+          onChange={formChangeHandler}
+        />
+        <br></br>
 
-    return(
-        <div className="Form">
-            <form onSubmit={formSubmitHandler}>
-               <label htmlFor="memberName">Name: </label>
-                <input 
-                type="text"
-                name="memberName"
-                value={formState.memberName}
-                placeholder="Enter a name"
-                onChange={formChangeHandler}
-                 />
-                
-                <br></br>
-                
-                <label htmlFor="email">Email: </label>
-                <input 
-                type="text"
-                value={formState.email} 
-                name= "email"
-                placeholder="Enter an Email Address"
-                onChange={formChangeHandler}
-    
-                />
-                <br></br>
-                
-                <label>Role: </label>
-                <input 
-                type="text"
-                value={formState.role}
-                name= "role"
-                placeholder="Enter a Role"
-                onChange={formChangeHandler} 
-                />
-                <br></br>
+        <label>Role: </label>
+        <input
+          type="text"
+          value={formState.role}
+          name="role"
+          placeholder="Enter a Role"
+          onChange={formChangeHandler}
+        />
+        <br></br>
 
-                <input type="submit" value="Submit"></input>
-            </form>    
-        </div>
-    )
-
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
